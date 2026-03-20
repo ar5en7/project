4bb62ca1a08d515d9eae3ebe09820114021b50c8
arsen1111111111 <253454@astanait.edu.kz>
2026-03-20 08:30:25 +0600

    task 1

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate(n):
-    if n == 0: return 
-    print(n,end=" ")
-    calculate(n-1)
-calculate(5)
+def array(n):
+    if len(n) == 1: return n[0]
+    
+    max = array(n[1::])
+
+    if n[0] > max:
+        return n[0]
+    else:
+        return max
+
+print(array([4,9,1,7,3]))
